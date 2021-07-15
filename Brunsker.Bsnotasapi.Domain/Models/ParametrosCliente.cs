f1f6a4c15930ab7d5006acd1afbcf7a10d63719d
rodrigo <rodrigo.rabelo@brunsker.com.br>
2021-07-15 15:23:56 -0300

    Adicionado novos parametros cliente

--- Brunsker.Bsnotasapi.Domain/Models/ParametrosCliente.cs
+++ Brunsker.Bsnotasapi.Domain/Models/ParametrosCliente.cs
@@ -12,9 +12,16 @@ namespace Brunsker.Bsnotasapi.Domain.Models
         public string VALIDARFINANPED { get; set; }
         public string MSGALIQXMLPEDDIV { get; set; }
         public string VALIDARNUMREGIAOSTGUIA { get; set; }
-        public int CODSTGUIA { get; set; }
+        public long CODSTGUIA { get; set; }
         public string MSGSEMSTNFPED { get; set; }
-        public int NUMREGVALIDASTGUIA { get; set; }
+        public long NUMREGVALIDASTGUIA { get; set; }
         public string MOVESTOQUENFLIVROFISCAL { get; set; }
+        public long CODCONTALANCNFCONSUMO { get; set; }
+        public long NUMBANCOLANCNFCONSUMO { get; set; }
+        public string GERABONUSENTNF { get; set; }
+        public string CODCONTASPED { get; set; }
+        public string TIPODESCARGANFCONSUMO { get; set; }
+        public string GERARLIVROFISCALNFUSOCONSUMO { get; set; }
+        public long CODTRIBPISCOFINS { get; set; }
     }
 }
