df4f87d12752e035d01d8cda3f234b78b22ffb65
Rodrigo <rodrigo_alexandre_rabelo@outlook.com>
2021-07-24 13:25:07 -0300

    24-07-2021

--- Brunsker.Bsnotasapi.Domain/Models/TotalizadorSaida.cs
+++ Brunsker.Bsnotasapi.Domain/Models/TotalizadorSaida.cs
@@ -4,8 +4,9 @@ namespace Brunsker.Bsnotasapi.Domain.Models
     {
         public double? VLTOTAL { get; set; }
         public long QTNFCANCELADAS { get; set; }
-        public long QTNF_ERP { get; set; }
+        public long QTNFERP { get; set; }
         public long QTNFDENEGADAS { get; set; }
         public long QTNFDEVOLUCAO { get; set; }
+        public long QTNFTRANSFERENCIA { get; set; }
     }
 }
