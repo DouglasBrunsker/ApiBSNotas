b787110a9af4438b9903d23340511d1b46f5a9cc
Matheus Duarte <matheus.duarte@brunsker.com.br>
2022-02-16 12:51:36 -0300

    add model fornecedores associados

---
+++ Brunsker.Bsnotasapi.Domain/Models/FornecedoresAssociados.cs
@@ -0,0 +1,9 @@
+namespace Brunsker.Bsnotasapi.Domain.Models
+{
+    public class FornecedoresAssociados
+    {
+        public int CODFORNEC { get; set; }
+        public string FORNECEDOR { get; set; }
+        public string CNPJ { get; set; }
+    }
+}
